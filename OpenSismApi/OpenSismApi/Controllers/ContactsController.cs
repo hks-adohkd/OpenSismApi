4cb1173c258ed3cbe05a5b84243785ee320f07a3
mhdYazbek <mohammed.yazbek.5@gmail.com>
2021-05-07 15:34:01 -0400

    added contact us and content about us controllers

---
+++ OpenSismApi/OpenSismApi/Controllers/ContactsController.cs
@@ -0,0 +1,54 @@
+﻿using OpenSismApi.AppStart;
+using OpenSismApi.Helpers;
+using OpenSismApi.Models;
+using AutoMapper;
+using DBContext.Models;
+using DBContext.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Linq;
+using X.PagedList;
+
+
+//this is our informations of whats up instgrame .....
+namespace OpenSismApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : BaseController
+    {
+        private readonly OpenSismDBContext _context;
+
+        public ContactsController(OpenSismDBContext context,
+            IStringLocalizer<BaseController> localizer) : base(localizer)
+        {
+            _context = context;
+        }
+
+        // GET: api/Contacts
+        [HttpPost]
+        [Route("GetAll")]
+        public Response<PagedContent<IPagedList<ContactViewModel>>> GetAll([FromBody] PaginationViewModel pagination)
+        {
+            Response<PagedContent<IPagedList<ContactViewModel>>> response = new Response<PagedContent<IPagedList<ContactViewModel>>>();
+            try
+            {
+                var items = _context.Contacts.Where(a => !a.IsDeleted)
+                    .ToPagedList(pageNumber: pagination.Page, pageSize: pagination.Limit);
+                var contacts = Mapper.Map<IPagedList<ContactViewModel>>(items);
+                PagedContent<IPagedList<ContactViewModel>> pagedContent = new PagedContent<IPagedList<ContactViewModel>>();
+                pagedContent.content = contacts;
+                pagedContent.pagination = new Pagination(contacts.TotalItemCount, contacts.PageSize, contacts.PageCount, contacts.PageNumber);
+                response = APIContants<PagedContent<IPagedList<ContactViewModel>>>.CostumSuccessResult(pagedContent);
+                return response;
+            }
+            catch (Exception e)
+            {
+                response = APIContants<PagedContent<IPagedList<ContactViewModel>>>.CostumSometingWrong(_localizer["SomethingWentWrong"], null);
+                Serilog.Log.Fatal(e, "{@RequestId}, {@Response}", CustomFilterAttribute.RequestId, response);
+                return response;
+            }
+        }
+    }
+}
---
+++ OpenSismApi/OpenSismApi/Controllers/SportMatchesController.cs
@@ -0,0 +1,135 @@
+﻿using OpenSismApi.AppStart;
+using OpenSismApi.Helpers;
+using OpenSismApi.Models;
+using AutoMapper;
+using DBContext.Models;
+using DBContext.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenSismApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class SportMatchesController : BaseController
+    {
+        private readonly OpenSismDBContext _context;
+
+        public SportMatchesController(OpenSismDBContext context,
+            IStringLocalizer<BaseController> localizer) : base(localizer)
+        {
+            _context = context;
+        }
+
+        // GET: api/Questions
+        [HttpPost]
+        [Route("Get")]
+        public Response<SportMatchViewModel> Get([FromBody] SportMatchViewModel model)
+        {
+            Response<SportMatchViewModel> response = new Response<SportMatchViewModel>();
+            try
+            {
+                var username = User.Identity.Name;
+                var customer = _context.Customers.Where(c => c.User.UserName == username).FirstOrDefault();
+                var item = _context.SportMatches.Include(s => s.AppTask).Include(s => s.AppTask.CustomerTasks)
+                    .Where(a => a.AppTaskId == model.AppTaskId && !a.IsDeleted && !a.AppTask.IsDeleted).FirstOrDefault();
+                if (item == null)
+                {
+                    response = APIContants<SportMatchViewModel>.CostumNotFound(_localizer["NotFound"], null);
+                    Serilog.Log.Warning("{@AddressId}, {@RequestId}, {@Response}", model.Id, CustomFilterAttribute.RequestId, response);
+                    return response;
+                }
+                var sportMatch = Mapper.Map<SportMatchViewModel>(item);
+                sportMatch.AppTask.IsDone = false;
+                item.AppTask.CustomerTasks = item.AppTask.CustomerTasks.Where(c => c.CustomerId == customer.Id)
+                    .Where(c => c.AppTask.TaskType.Name != "share_games_app").ToList();
+                if (item.AppTask.CustomerTasks.Count() > 0)
+                {
+                    foreach (var ct in item.AppTask.CustomerTasks)
+                    {
+                        if (ct.IsDone)
+                        {
+                            sportMatch.AppTask.IsDone = true;
+                            break;
+                        }
+                    }
+                }
+                int count = _context.CustomerTasks.Where(c => c.IsDone && c.AppTaskId == item.Id).Count();
+                if (count < item.AppTask.Limit)
+                    sportMatch.AppTask.IsReachLimit = false;
+                else
+                    sportMatch.AppTask.IsReachLimit = true;
+                response = APIContants<SportMatchViewModel>.CostumSuccessResult(sportMatch, customer);
+                return response;
+            }
+            catch (Exception e)
+            {
+                response = APIContants<SportMatchViewModel>.CostumSometingWrong(_localizer["SomethingWentWrong"], null);
+                Serilog.Log.Fatal(e, "{@RequestId}, {@Response}", CustomFilterAttribute.RequestId, response);
+                return response;
+            }
+        }
+
+        [HttpPost]
+        [Route("AddEnd")]
+        public async Task<Response<CustomerTaskViewModel>> AddEnd([FromBody] CustomerPredictionViewModel model)
+        {
+            Response<CustomerTaskViewModel> response = new Response<CustomerTaskViewModel>();
+            try
+            {
+                var username = User.Identity.Name;
+                var customer = _context.Customers.Where(c => c.User.UserName == username).FirstOrDefault();
+
+                SportMatch sportMatch = _context.SportMatches.Find(model.SportMatchId);
+
+                CustomerTask customerTask = _context.CustomerTasks.Where(c => c.CustomerId == customer.Id
+                && c.AppTaskId == sportMatch.AppTaskId).FirstOrDefault();
+
+                if(customerTask == null)
+                {
+                    customerTask = new CustomerTask();
+                    customerTask.CustomerId = customer.Id;
+                    customerTask.AppTaskId = sportMatch.AppTaskId;
+                    customerTask.DoneDate = DateTime.Now;
+                    customerTask.IsDone = true;
+                    customerTask.StartDate = DateTime.Now;
+                    customerTask.EarnedPoints = 0;
+
+                    _context.CustomerTasks.Add(customerTask);
+                    await _context.SaveChangesAsync();
+                }
+                else
+                {
+                    customerTask.DoneDate = DateTime.Now;
+                    customerTask.IsDone = true;
+
+                    _context.CustomerTasks.Update(customerTask);
+                    await _context.SaveChangesAsync();
+                }
+
+                CustomerPrediction customerPrediction = new CustomerPrediction();
+                customerPrediction.CustomerId = customer.Id;
+                customerPrediction.FirstTeamScore = model.FirstTeamScore;
+                customerPrediction.SecondTeamScore = model.SecondTeamScore;
+                customerPrediction.SportMatchId = sportMatch.Id;
+                _context.CustomerPredictions.Add(customerPrediction);
+                await _context.SaveChangesAsync();
+
+                response = APIContants<CustomerTaskViewModel>.CostumSuccessResult(Mapper.Map<CustomerTaskViewModel>(customerTask), customer);
+                return response;
+            } 
+            catch (Exception e)
+            {
+                response = APIContants<CustomerTaskViewModel>.CostumSometingWrong(_localizer["SomethingWentWrong"], null);
+                Serilog.Log.Fatal(e, "{@RequestId}, {@Response}", CustomFilterAttribute.RequestId, response);
+                return response;
+            }
+        }
+    }
+}
