55412764f36358e187712a6b190c59c51c08e02c
mhdYazbek <mohammed.yazbek.5@gmail.com>
2021-05-08 14:15:10 -0400

    fixed locally connection with server

--- OpenSismApi/OpenSismApi/Controllers/CitiesControllers.cs
+++ OpenSismApi/OpenSismApi/Controllers/CitiesControllers.cs
@@ -53,5 +53,34 @@ namespace OpenSismApi.Controllers
         }
 
 
+        [HttpGet]
+        [Route("Get")]
+        public Response<PagedContent<IPagedList<CityViewModel>>> Get([FromBody] PaginationViewModel pagination)
+        {
+            Response<PagedContent<IPagedList<CityViewModel>>> response = new Response<PagedContent<IPagedList<CityViewModel>>>();
+            try
+            {
+                var items = _context.Cities.Where(a => !a.IsDeleted)
+                    .OrderBy(c => c.DisplayName)
+                    .ToPagedList(pageNumber: pagination.Page, pageSize: pagination.Limit);
+
+                var cities = Mapper.Map<IPagedList<CityViewModel>>(items);
+                // var cities = _mapper.Map<IPagedList<CityViewModel>>(items);
+                PagedContent<IPagedList<CityViewModel>> pagedContent = new PagedContent<IPagedList<CityViewModel>>();
+                pagedContent.content = cities;
+                pagedContent.pagination = new Pagination(cities.TotalItemCount, cities.PageSize, cities.PageCount, cities.PageNumber);
+                response = APIContants<PagedContent<IPagedList<CityViewModel>>>.CostumSuccessResult(pagedContent);
+                return response;
+            }
+            catch (Exception e)
+            {
+                response = APIContants<PagedContent<IPagedList<CityViewModel>>>.CostumSometingWrong(_localizer["SomethingWentWrong"], null);
+                //  Serilog.Log.Fatal(e, "{@RequestId}, {@Response}", CustomFilterAttribute.RequestId, response);
+                return response;
+            }
+        }
+
+
+
     }
 }
