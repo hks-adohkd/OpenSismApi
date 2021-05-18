567d4913f558a21a264fc506e319421c5faad65f
MhdYazbek <mohammed.yazbek.5@gmail.com>
2021-05-17 23:31:15 -0500

    added migration for work pc

---
+++ OpenSismApi/DBContext/Migrations/20210518042817_refresh_pc_work.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DBContext.Migrations
+{
+    public partial class refresh_pc_work : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
